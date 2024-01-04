b26de1de632d4e069c6208b7b70718482020ad4b
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2024-01-04 23:57:09 +0530

    worked in the createQauotation page. faced errors

--- src/pages/enquiries/create-quotation.js
+++ src/pages/enquiries/create-quotation.js
@@ -1,159 +1,183 @@
-import React, { useState, useEffect } from 'react';
-import { Form, Button, Row, Col, Alert, Table, InputGroup, FormControl } from 'react-bootstrap';
+import React,{ useState,useEffect } from 'react';
+import { Form,Button,Row,Col,Alert,Table,FormControl } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 const CreateQuotation = () => {
-  const [customer_id, setUserId] = useState('');
-  const [date, setDateOfEnquiry] = useState(new Date());
-  const [remarks, setRemarks] = useState('');
-  const [successMessage, setSuccessMessage] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [allEnquiries, setAllEnquiries] = useState([]);
-  const [selectedProducts, setSelectedProducts] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [customer_id,setUserId] = useState('');
+  const [date,setDateOfEnquiry] = useState(new Date());
+  const [remarks,setRemarks] = useState('');
+  const [successMessage,setSuccessMessage] = useState('');
+  const [errorMessage,setErrorMessage] = useState('');
+  const [allEnquiries,setAllEnquiries] = useState([]);
+  const [quotedProducts,setQuotedProducts] = useState([]);
+  const [selectedEnquiryDetails,setSelectedEnquiryDetails] = useState(null);
+  const [totalAmount,setTotalAmount] = useState(0);
 
-  const handleProductAction = (product, quantity = 1) => {
-    const isProductSelected = selectedProducts.find((selected) => selected.product_id === product.product_id);
-    const productWithPrice = { ...product, price: 0 };
-
-    setSelectedProducts([...selectedProducts, { ...productWithPrice, quantity }]);
-    
-    if (isProductSelected) {
-      const updatedSelectedProducts = selectedProducts.filter((selected) => selected.product_id !== product.product_id);
-      setSelectedProducts(updatedSelectedProducts);
+  const fetchEnquiries = async () => {
+    try {
+      const response = await fetch('/api/potentialCustomers/fetch-all-enquiries');
+      if (response.ok) {
+        const data = await response.json();
+        setAllEnquiries(data);
+      } else {
+        console.error('Error fetching enquiries:',response.status,response.statusText);
+      }
+    } catch (error) {
+      console.error('Error fetching enquiries:',error);
     }
-  };
-
-  const handleRowClick = (clickedProduct) => {
-    const isProductSelected = selectedProducts.some((product) => product.product_id === clickedProduct.product_id);
-
-    if (isProductSelected) {
-      handleIncreaseQuantity(clickedProduct);
-    } else {
-      const productWithPrice = { ...clickedProduct, price: 0 };
-      handleProductAction(productWithPrice, 1);
-    }
-  };
-
-  const handlePriceChange = (index, event) => {
-    const newSelectedProducts = [...selectedProducts];
-    const newPrice = parseFloat(event.target.value);
-
-    if (!isNaN(newPrice) && newPrice >= 0) {
-      newSelectedProducts[index].price = newPrice;
-      setSelectedProducts(newSelectedProducts);
-    }
-  };
-
-  const handleIncreaseQuantity = (product) => {
-    const updatedSelectedProducts = selectedProducts.map((selected) =>
-      selected.product_id === product.product_id ? { ...selected, quantity: selected.quantity + 1 } : selected
-    );
-    setSelectedProducts(updatedSelectedProducts);
   };
 
   useEffect(() => {
-    const fetchEnquiries = async () => {
-      try {
-        const response = await fetch('/api/potentialCustomers/fetch-all-enquiries');
-        if (response.ok) {
-          const data = await response.json();
-          setAllEnquiries(data);
-        } else {
-          console.error('Error fetching enquiries:', response.status, response.statusText);
-        }
-      } catch (error) {
-        console.error('Error fetching enquiries:', error);
-      }
-    };
-
     fetchEnquiries();
-  }, []);
+  },[]);
+
+  const handleRowDoubleClick = async (enquiryId) => {
+    try {
+      const response = await fetch(`/api/potentialCustomers/fetch-enquiry-details?enquiryId=${enquiryId}`);
+      if (response.ok) {
+        const data = await response.json();
+        setSelectedEnquiryDetails({
+          enquiry_id: data[0].enquiry_id,
+          customer_name: data[0].potential_customers.name,
+          customer_id: data[0].potential_customers.customer_id,
+          date: data[0].date_of_enquiry,
+        });
+        setQuotedProducts(data);
+      } else {
+        console.error('Error fetching enquiry details:',response.status,response.statusText);
+      }
+    } catch (error) {
+      console.error('Error fetching enquiry details:',error);
+    }
+  };
+
+  const calculateAmount = (quotedProduct) => {
+    return (quotedProduct.cost || 0) * quotedProduct.quantity;
+  };
+
+  const calculateTotalAmount = () => {
+    return quotedProducts.reduce((total,quotedProduct) => total + calculateAmount(quotedProduct),0).toFixed(3);
+  };
+
+  const handleCostChange = (index,event) => {
+    const newQuotedProducts = [...quotedProducts];
+    const newCost = parseFloat(event.target.value);
+
+    if (!isNaN(newCost) && newCost >= 0) {
+      newQuotedProducts[index].cost = newCost;
+      setQuotedProducts(newQuotedProducts);
+    }
+  };
+
+  const handleClearQuotedProducts = () => {
+    setQuotedProducts([]);
+    setTotalAmount(0);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const userIdRegex = /^\d{6}$/;
-    if (!userIdRegex.test(customer_id)) {
-      setSuccessMessage('');
-      setErrorMessage('User ID must be exactly 6 digits.');
-      return;
-    }
-
     try {
-      const response = await fetch('/api/potentialCustomers/add-quotation', {
+      // Form validation logic...
+
+      // Step 1: Call the add-quotation API
+      const addQuotationResponse = await fetch('/api/potentialCustomers/add-quotation',{
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ customer_id, date, enquiry_id }),
+        body: JSON.stringify({
+          customer_id: selectedEnquiryDetails.customer_id,
+          date: selectedEnquiryDetails.date,
+          enquiry_id: selectedEnquiryDetails.enquiry_id,
+        }),
       });
 
-      if (response.ok) {
-        const data = await response.json();
+      if (addQuotationResponse.ok) {
+        // Step 2: Call the add-quotation-pricing API
+        const addQuotationData = await addQuotationResponse.json();
+        console.log(quotedProducts.map((quotedProduct) => ({
+          product_id: quotedProduct.products.product_id,
+          price_tie: quotedProduct.cost,
+          quantity: quotedProduct.quantity,
+        })));
+        const addQuotationPricingResponse = await fetch('/api/potentialCustomers/add-quotation-pricing',{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            customer_id: selectedEnquiryDetails.customer_id,
+            enquiry_id: addQuotationData.enquiry_id,
+            products: quotedProducts.map((quotedProduct) => ({
+              product_id: quotedProduct.products.product_id,
+              price_tie: quotedProduct.cost,
+              quantity: quotedProduct.quantity,
+            })),
+          }),
+        });
 
-        const postData = {
-          customer_id: customer_id,
-          enquiry_id: data.enquiry_id,
-          products: selectedProducts,
-        };
-
-        try {
-          const postResponse = await fetch('/api/potentialCustomers/add-interests-of-customer', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(postData),
-          });
-
-          if (postResponse.ok) {
-            setTimeout(() => {
-              setSuccessMessage(`Enquiry created successfully with ID: ${data.intrest_id}`);
-            }, 3000);
-          } else {
-            const errorData = await postResponse.json();
-            setErrorMessage(`Error: ${errorData.message}`);
-          }
-        } catch (error) {
-          console.error('Error:', error);
-          setErrorMessage('Internal Server Error');
+        if (addQuotationPricingResponse.ok) {
+          // Both APIs were successful
+          setSuccessMessage(`Quotation created successfully with ID: ${addQuotationData.enquiry_id}`);
+        } else {
+          // Error in add-quotation-pricing API
+          const errorData = await addQuotationPricingResponse.json();
+          setSuccessMessage('');
+          setErrorMessage(`Error: ${errorData.message}`);
         }
-      } else {
-        const errorData = await response.json();
-        setSuccessMessage('');
-        setErrorMessage(`Error: ${errorData.message}`);
       }
     } catch (error) {
-      console.error('Error creating Enquiry:', error);
+      console.error('Unexpected Error in handleSubmit:',error);
       setSuccessMessage('');
-      setErrorMessage('Internal Server Error');
     }
+    //   if (!addQuotationResponse.ok) {
+    //     const errorData = await addQuotationResponse.json();
+    //     setSuccessMessage('');
+    //     setErrorMessage(`Error: ${errorData.message}`);
+    //     return;
+    //   }
+
+    //   const addQuotationData = await addQuotationResponse.json();
+
+    //   // Step 2: Call the add-quotation-pricing API
+    //   const addQuotationPricingResponse = await fetch('/api/potentialCustomers/add-quotation-pricing',{
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //       customer_id: selectedEnquiryDetails.customer_id,
+    //       enquiry_id: addQuotationData.enquiry_id,
+    //       products: quotedProducts.map((quotedProduct) => ({
+    //         product_id: quotedProduct.products.product_id,
+    //         price_tie: quotedProduct.cost,
+    //         quantity: quotedProduct.quantity,
+    //       })),
+    //     }),
+    //   });
+
+    //   if (addQuotationPricingResponse.ok) {
+    //     // Both APIs were successful
+    //     setSuccessMessage(`Quotation created successfully with ID: ${addQuotationData.enquiry_id}`);
+    //   } else {
+    //     // Error in add-quotation-pricing API
+    //     const errorData = await addQuotationPricingResponse.json();
+    //     setSuccessMessage('');
+    //     setErrorMessage(`Error: ${errorData.message}`);
+    //   }
+    // } catch (error) {
+    //   console.error('Unexpected Error in handleSubmit:',error);
+    //   setSuccessMessage('');
+    //   setErrorMessage('Internal Server Error');
+    // }
   };
 
-  const handleClearSelected = () => {
-    setSelectedProducts([]);
-  };
-
-  const handleQuantityChange = (index, event) => {
-    const newSelectedProducts = [...selectedProducts];
-    const newQuantity = parseInt(event.target.value, 10);
-
-    if (!isNaN(newQuantity) && newQuantity >= 0) {
-      newSelectedProducts[index].quantity = newQuantity;
-      setSelectedProducts(newSelectedProducts);
-    }
-  };
-
-  const handleRemoveProduct = (product) => {
-    const updatedSelectedProducts = selectedProducts.filter((selected) => selected.product_id !== product.product_id);
-    setSelectedProducts(updatedSelectedProducts);
-  };
 
   return (
     <div>
-      <h2>Create Enquiry</h2>
+      <h2>Create Quotation</h2>
 
       {successMessage && <Alert variant="success">{successMessage}</Alert>}
       {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
@@ -177,85 +201,21 @@ const CreateQuotation = () => {
           <Col md={6}>
             <Form.Group controlId="date">
               <Form.Label>Date </Form.Label>
-              <div className="input-group">
-                <DatePicker
-                  selected={date}
-                  onChange={(date) => setDateOfEnquiry(date)}
-                  dateFormat="MMMM d, yyyy"
-                  className="form-control"
-                  required
-                />
-              </div>
+              <DatePicker
+                selected={date}
+                onChange={(date) => setDateOfEnquiry(date)}
+                dateFormat="MMMM d, yyyy"
+                className="form-control"
+                required
+              />
             </Form.Group>
           </Col>
         </Row>
 
         <Row>
-          <Col md={6}>
-            <h4>Selected Products</h4>
-            <Table striped bordered hover style={{ maxHeight: '300px', minHeight: '300px', overflowY: 'auto', fontSize: '0.8rem' }}>
-              <thead>
-                <tr>
-                  <th style={{ width: '15%' }}>Prod. ID</th>
-                  <th style={{ width: '25%' }}>Prod. Name</th>
-                  <th style={{ minWidth: '20%' }}>Quantity</th>
-                  <th style={{ minWidth: '15%' }}>Price</th>
-                  <th style={{ minWidth: '1%' }}></th>
-                </tr>
-              </thead>
-              <tbody>
-                {selectedProducts.length === 0 ? (
-                  <tr>
-                    <td colSpan="5" className="text-center">
-                      Table is empty
-                    </td>
-                  </tr>
-                ) : (
-                  selectedProducts.map((selectedProduct, index) => (
-                    <tr key={selectedProduct.product_id}>
-                      <td>{selectedProduct.product_id}</td>
-                      <td>{selectedProduct.product_name}</td>
-                      <td>
-                        <FormControl
-                          type="number"
-                          step="1"
-                          value={selectedProduct.quantity}
-                          onChange={(e) => handleQuantityChange(index, e)}
-                          style={{ fontSize: '0.8rem' }}
-                        />
-                      </td>
-                      <td>
-                        <FormControl
-                          type="number"
-                          step="0.01"
-                          value={selectedProduct.price || 0}
-                          onChange={(e) => handlePriceChange(index, e)}
-                          style={{ fontSize: '0.8rem' }}
-                        />
-                      </td>
-                      <td>
-                        <Button
-                          variant="light"
-                          size="sm"
-                          style={{ fontSize: '0.8rem', margin: '0', padding: '0' }}
-                          onClick={() => handleRemoveProduct(selectedProduct)}
-                        >
-                          ❌
-                        </Button>
-                      </td>
-                    </tr>
-                  ))
-                )}
-              </tbody>
-            </Table>
-            <Button variant="danger" onClick={handleClearSelected}>
-              Clear Selected
-            </Button>
-          </Col>
-
-          <Col md={6}>
+          <Col md={12}>
             <h4>All Enquiries</h4>
-            <Table striped bordered hover style={{ maxHeight: '250px', minHeight: '250px', overflowY: 'auto', fontSize: '0.8rem' }}>
+            <Table striped bordered hover style={{ maxHeight: '250px',minHeight: '250px',overflowY: 'auto',fontSize: '0.8rem' }}>
               <thead>
                 <tr>
                   <th>Enquiry ID</th>
@@ -267,24 +227,80 @@ const CreateQuotation = () => {
                 </tr>
               </thead>
               <tbody>
-                {allEnquiries
-                  .map((enquiry) => (
-                    <tr
-                      key={enquiry.enquiry_id}
-                      onClick={() => {
-                        handleRowClick(enquiry);
-                      }}
-                    >
-                      <td>{enquiry.enquiry_id}</td>
-                      <td>{enquiry.remarks}</td>
-                      <td>{enquiry.date_of_enquiry}</td>
-                      <td>{enquiry.potential_customers.name}</td>
-                      <td>{enquiry.potential_customers.customer_id}</td>
-                      <td>{enquiry.potential_customers.phone_number}</td>
-                    </tr>
-                  ))}
+                {allEnquiries.map((enquiry) => (
+                  <tr
+                    key={enquiry.enquiry_id}
+                    onDoubleClick={() => handleRowDoubleClick(enquiry.enquiry_id)}
+                  >
+                    <td>{enquiry.enquiry_id}</td>
+                    <td>{enquiry.remarks}</td>
+                    <td>{enquiry.date_of_enquiry}</td>
+                    <td>{enquiry.potential_customers.name}</td>
+                    <td>{enquiry.potential_customers.customer_id}</td>
+                    <td>{enquiry.potential_customers.phone_number}</td>
+                  </tr>
+                ))}
               </tbody>
             </Table>
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md={12}>
+            <h4>Quoted Products</h4>
+            {/* Display Enquiry ID, Customer Name, Customer ID, and Date */}
+            {selectedEnquiryDetails && (
+              <div style={{ display: 'flex',flexDirection: 'column',gap: '2px' }}>
+                <div>Enquiry ID: {selectedEnquiryDetails.enquiry_id}</div>
+                <div>Customer Name: {selectedEnquiryDetails.customer_name}</div>
+                <div>Customer ID: {selectedEnquiryDetails.customer_id}</div>
+                <div>Date: {selectedEnquiryDetails.date}</div>
+              </div>
+            )}
+
+            {/* Quoted Products table */}
+            <Table striped bordered hover style={{ maxHeight: '300px',minHeight: '300px',overflowY: 'auto',fontSize: '0.8rem' }}>
+              <thead>
+                <tr>
+                  <th>Product ID</th>
+                  <th>Product Name</th>
+                  <th>Quantity</th>
+                  <th>Cost</th>
+                  <th>Amount</th>
+                </tr>
+              </thead>
+              <tbody>
+                {quotedProducts.map((quotedProduct,index) => (
+                  <tr key={index}>
+                    <td>{quotedProduct.products.product_id}</td>
+                    <td>{quotedProduct.products.product_name}</td>
+                    <td>{quotedProduct.quantity}</td>
+                    <td>
+                      <FormControl
+                        type="number"
+                        step="0.001"
+                        value={quotedProduct.cost || 0}
+                        onChange={(e) => handleCostChange(index,e)}
+                        style={{ fontSize: '0.8rem' }}
+                      />
+                    </td>
+                    <td>{calculateAmount(quotedProduct)}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+
+            {/* Clear Quoted Products and Total Amount */}
+            <Row>
+              <Col md={6}>
+                <Button variant="danger" onClick={handleClearQuotedProducts}>
+                  Clear Quoted Products
+                </Button>
+              </Col>
+              <Col md={6} className="text-right">
+                <strong>Total Amount: {calculateTotalAmount()}</strong>
+              </Col>
+            </Row>
           </Col>
         </Row>
 
